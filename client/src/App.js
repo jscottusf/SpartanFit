9427a956d17f97950326294320c009447a681e1f
Mlipphardt <matthewlipphardt@gmail.com>
2020-05-03 19:10:52 -0400

    Routing added for 404 page and recipes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Recipes from "./components/Recipes";
+import NotFound from "./components/NotFound";
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">Hello, world</div>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route exact path="/recipes" component={Recipes} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </Router>
     );
   }
