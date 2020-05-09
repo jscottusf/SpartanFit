d42a132f81745c790905be9e7dd273f659846502
Joel Scott <jscottusf@gmail.com>
2020-05-09 10:00:30 -0400

    backend for user auth

--- database/models/user.js
+++ database/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs'),
-  SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+mongoose.promise = Promise;
 
 const validateEmail = email => {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -22,7 +22,7 @@ const userSchema = new Schema({
     ],
   },
   password: { type: String, required: true },
-  userName: {
+  username: {
     type: String,
     required: true,
     required: true,
@@ -40,34 +40,28 @@ const userSchema = new Schema({
   bio: { type: String },
 });
 
-userSchema.pre('save', function (next) {
-  var user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+// Define schema methods
+userSchema.methods = {
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password);
+  },
+  hashPassword: plainTextPassword => {
+    return bcrypt.hashSync(plainTextPassword, 10);
+  },
 };
 
-const User = mongoose.model('User', userSchema);
+// Define hooks for pre-saving
+userSchema.pre('save', function (next) {
+  if (!this.password) {
+    console.log('models/user.js =======NO PASSWORD PROVIDED=======');
+    next();
+  } else {
+    console.log('models/user.js hashPassword in pre save');
 
+    this.password = this.hashPassword(this.password);
+    next();
+  }
+});
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+//const bodyParser = require('body-parser');
+const dbConnection = require('./database');
+const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('./passport');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const MongoStore = require('connect-mongo')(session);
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//require routes
+//const user = require('./routes/user');
 
 // Define middleware
 app.use(cors());
@@ -15,11 +24,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-// Define API routes
-app.use(routes);
+// Sessions
+app.use(
+  session({
+    secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+    store: new MongoStore({ mongooseConnection: dbConnection }),
+    resave: false, //required
+    saveUninitialized: false, //required
+  })
+);
 
-// Connect to Mongo DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/spartanfit');
+// Passport
+app.use(passport.initialize());
+app.use(passport.session()); // calls the deserializeUser
+
+// Define API routes
+// Routes
+//app.use('/user', user);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
