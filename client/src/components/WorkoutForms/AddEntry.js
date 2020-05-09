ce485d55de1a8df4418c301e58358c3454bb168e
Mlipphardt <matthewlipphardt@gmail.com>
2020-05-09 16:43:04 -0400

    Data entry form completed.

--- client/src/components/WorkoutForms/AddEntry.js
+++ client/src/components/WorkoutForms/AddEntry.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function AddEntry(props) {
-  return <h5>Add entries here!</h5>;
+  return (
+    <form className="text-center">
+      <div className="form-group">
+        <label for="dataData">Date (MM/DD/YYYY)</label>
+        <input
+          type="text"
+          class="form-control"
+          id="dataDate"
+          name="dataDate"
+          placeholder="01/01/20..."
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <label for="dataValue">Value</label>
+        <input
+          class="form-control"
+          id="dataValue"
+          name="dataValue"
+          placeholder="1, 5, 20..."
+          onChange={props.handleInputChange}
+        />
+      </div>
+    </form>
+  );
 }
 
 export default AddEntry;
