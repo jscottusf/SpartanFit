0075b4b8331e0d512bb3e884cdce1917c380d922
Mlipphardt <matthewlipphardt@gmail.com>
2020-05-16 23:48:02 -0400

    Altered WorkoutController delete to account for referenced Data

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -35,8 +35,35 @@ module.exports = {
   },
   remove: function (req, res) {
     db.Workout.findById({ _id: req.params.id })
-      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => {
+        dbModel.remove();
+        return db.Data.remove({ _id: { $in: dbModel.data } });
+      })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
 };
+/*
+
+      "data": [
+        "5ebc95bc18fb423a240f9b0c",
+        "5ebdeb8d01246b15bce96e6a",
+        "5ebdeba001246b15bce96e6b",
+        "5ec06ae3278c95171c00ba88"
+    ],
+
+  remove: function (req, res) {
+    db.Data.findById({ _id: req.params.id })
+      .then((dbModel) => {
+        dbModel.remove();
+        return db.Workout.findOneAndUpdate(
+          { data: req.params.id },
+          { $pull: { data: req.params.id } }
+        ).then(function (dbWorkout) {
+          res.json(dbWorkout);
+        });
+      })
+      .catch((err) => res.status(422).json(err));
+  },
+};
+*/
