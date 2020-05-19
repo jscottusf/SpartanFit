44f85cde397279b56a277b2aced6b5169237bd44
Joel Scott <jscottusf@gmail.com>
2020-05-18 22:11:32 -0400

    added redirect to logout

--- client/src/components/NavMenu/index.js
+++ client/src/components/NavMenu/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Row } from '../../components/Grid';
 import { Link } from 'react-router-dom';
 import API from '../../utils/API';
@@ -19,6 +20,9 @@ class NavMenu extends Component {
     API.logout()
       .then(response => {
         if (response.status === 200) {
+          this.setState({
+            redirectTo: '/',
+          });
           this.props.updateUser({
             loggedIn: false,
             username: null,
@@ -31,80 +35,84 @@ class NavMenu extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="jumbotron jumbotron-fluid">
-          <div className="container">
-            <h1 className="topleft">
-              Spartan<span class="text-info">Fit</span>
-            </h1>
-            <div className="links">
-              <Row>
-                <Link
-                  to="/"
-                  className={
-                    window.location.pathname === '/' ||
-                    window.location.pathname === '/home'
-                      ? 'home-link active'
-                      : 'home-link'
-                  }
-                >
-                  Home <span className="sr-only">(current)</span>
-                </Link>
-                <Link
-                  to="/profile"
-                  className={
-                    window.location.pathname === '/profile'
-                      ? 'profile-link active'
-                      : 'profile-link'
-                  }
-                >
-                  Profile
-                </Link>
-                <Link
-                  to="/workouts"
-                  className={
-                    window.location.pathname === '/workouts'
-                      ? 'workouts-link active'
-                      : 'workouts-link'
-                  }
-                >
-                  Workouts
-                </Link>
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />;
+    } else {
+      return (
+        <div>
+          <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+              <h1 className="topleft">
+                Spartan<span class="text-info">Fit</span>
+              </h1>
+              <div className="links">
+                <Row>
+                  <Link
+                    to="/"
+                    className={
+                      window.location.pathname === '/' ||
+                      window.location.pathname === '/home'
+                        ? 'home-link active'
+                        : 'home-link'
+                    }
+                  >
+                    Home <span className="sr-only">(current)</span>
+                  </Link>
+                  <Link
+                    to="/profile"
+                    className={
+                      window.location.pathname === '/profile'
+                        ? 'profile-link active'
+                        : 'profile-link'
+                    }
+                  >
+                    Profile
+                  </Link>
+                  <Link
+                    to="/workouts"
+                    className={
+                      window.location.pathname === '/workouts'
+                        ? 'workouts-link active'
+                        : 'workouts-link'
+                    }
+                  >
+                    Workouts
+                  </Link>
 
-                <div className="dropdown recipes-link">
-                  <div
-                    className="dropdown-toggle"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                    style={{ cursor: 'pointer' }}
-                  >
-                    Recipes
+                  <div className="dropdown recipes-link">
+                    <div
+                      className="dropdown-toggle"
+                      id="navbarDropdown"
+                      role="button"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                      style={{ cursor: 'pointer' }}
+                    >
+                      Recipes
+                    </div>
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                      <Link className="dropdown-item" to="/savedrecipes">
+                        My Recipes
+                      </Link>
+                      <Link to="/recipes" className="dropdown-item">
+                        <span className="text-secondary">Search Recipes</span>
+                      </Link>
+                    </div>
                   </div>
-                  <div
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <Link className="dropdown-item" to="/savedrecipes">
-                      My Recipes
-                    </Link>
-                    <Link to="/recipes" className="dropdown-item">
-                      <span className="text-secondary">Search Recipes</span>
-                    </Link>
-                  </div>
-                </div>
-                <Link className="logout-link" to="/" onClick={this.logout}>
-                  logout
-                </Link>
-              </Row>
+                  <Link className="logout-link" to="/" onClick={this.logout}>
+                    logout
+                  </Link>
+                </Row>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
