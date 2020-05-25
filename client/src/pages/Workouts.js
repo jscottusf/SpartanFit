2c1a0a40d36b1d1f95ff6b880344a3ce3e328399
Joel Scott <jscottusf@gmail.com>
2020-05-25 11:04:01 -0400

    redesigned workoutcards to match page design. Will likely replace buttons with fontawesome icons

--- client/src/pages/Workouts.js
+++ client/src/pages/Workouts.js
@@ -1,27 +1,33 @@
-import React, { Component } from "react";
-import API from "../utils/API";
-import Modal from "../components/Modal";
-import AddEntry from "../components/WorkoutModalBodies/AddEntry";
-import AddWorkout from "../components/WorkoutModalBodies/AddWorkout";
-import ViewEntries from "../components/WorkoutModalBodies/ViewEntries";
-import ViewChart from "../components/WorkoutModalBodies/ViewChart";
-import Wrapper from "../components/Wrapper";
-import WorkoutCard from "../components/WorkoutCard";
-import { format } from "date-fns";
-import { Line } from "react-chartjs-2";
-import "./workouts.css";
-import { Col, Row, Container } from "../components/Grid";
+import React, { Component } from 'react';
+import API from '../utils/API';
+import Modal from '../components/Modal';
+import AddEntry from '../components/WorkoutModalBodies/AddEntry';
+import AddWorkout from '../components/WorkoutModalBodies/AddWorkout';
+import ViewEntries from '../components/WorkoutModalBodies/ViewEntries';
+import ViewChart from '../components/WorkoutModalBodies/ViewChart';
+import WorkoutCard from '../components/WorkoutCard';
+import { format } from 'date-fns';
+import { Line } from 'react-chartjs-2';
+import './workouts.css';
+import { Col, Row, Container } from '../components/Grid';
+import {
+  CardDiv,
+  CardBody,
+  CardTitle,
+  CardText,
+} from '../components/BootstrapCard';
+import GridContainer from '../components/GridContainer';
 
 class Workouts extends Component {
   state = {
     workouts: [],
-    dataDate: "",
-    dataValue: "",
-    workoutName: "",
-    workoutDescription: "",
-    workoutType: "",
-    modalForm: "",
-    entryID: "",
+    dataDate: '',
+    dataValue: '',
+    workoutName: '',
+    workoutDescription: '',
+    workoutType: '',
+    modalForm: '',
+    entryID: '',
     retrievedEntries: [],
   };
 
@@ -30,16 +36,16 @@ class Workouts extends Component {
   };
 
   //Changes modal body to be the add-entry form for inputting data.
-  addEntry = (id) => {
-    this.setState({ modalForm: "add-entry", entryID: id });
+  addEntry = id => {
+    this.setState({ modalForm: 'add-entry', entryID: id });
   };
 
   addWorkout = () => {
-    this.setState({ modalForm: "add-workout" });
+    this.setState({ modalForm: 'add-workout' });
   };
 
   //Used for finding Stepsize for Chart.js from passed data.
-  cleanStepsize = (arr) => {
+  cleanStepsize = arr => {
     if (arr.length > 2) {
       return Math.floor(
         (Math.max(arr[0].value, arr[1].value, arr[2].value) -
@@ -58,7 +64,7 @@ class Workouts extends Component {
   };
 
   //Deletes data when button is clicked.
-  handleDeleteEntry = (id) => {
+  handleDeleteEntry = id => {
     console.log(`Deleting entry with ID: ${id}`);
     API.deleteData(id).then((res, err) => {
       if (err) {
@@ -70,7 +76,7 @@ class Workouts extends Component {
   };
 
   //Deletes workout and associated data when button is clicked.
-  handleDeleteWorkout = (id) => {
+  handleDeleteWorkout = id => {
     API.deleteWorkout(id).then((res, err) => {
       if (err) {
         console.log(err);
@@ -79,7 +85,7 @@ class Workouts extends Component {
     });
   };
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
@@ -89,9 +95,9 @@ class Workouts extends Component {
   //Submits form for both workouts and data entry
   handleFormSubmit = () => {
     switch (this.state.modalForm) {
-      case "add-workout":
+      case 'add-workout':
         return this.submitWorkouts();
-      case "add-entry":
+      case 'add-entry':
         return this.submitData(this.state.entryID);
       default:
         return null;
@@ -99,7 +105,7 @@ class Workouts extends Component {
   };
 
   //Grabs just one workout's information, used for grabbing more data to put in modals
-  loadOneWorkout = (id) => {
+  loadOneWorkout = id => {
     API.getWorkoutByID(id).then((res, err) => {
       if (err) {
         console.log(err);
@@ -121,20 +127,20 @@ class Workouts extends Component {
   };
 
   //Chooses contents of modal based on state
-  selectForm = (form) => {
+  selectForm = form => {
     switch (form) {
-      case "add-workout":
+      case 'add-workout':
         return <AddWorkout handleInputChange={this.handleInputChange} />;
-      case "add-entry":
+      case 'add-entry':
         return <AddEntry handleInputChange={this.handleInputChange} />;
-      case "view-entries":
+      case 'view-entries':
         return (
           <ViewEntries
             delete={this.handleDeleteEntry}
             data={this.state.retrievedEntries}
           />
         );
-      case "view-chart":
+      case 'view-chart':
         return <ViewChart data={this.state.retrievedEntries} />;
       default:
         return null;
@@ -189,124 +195,173 @@ class Workouts extends Component {
 
   render() {
     return (
-      <Container>
-        <div>
-          <div className="workouts">
-            <Modal
-              show={this.state.show}
-              close={this.hideModal}
-              submit={this.handleFormSubmit}
-              //Changes body of modal based on state. Allows for modal reuse.
-              status={this.state.modalForm}
-            >
-              {this.selectForm(this.state.modalForm, this.state.entryID)}
-            </Modal>
-            <Wrapper>
-              <div className="main-container">
-                <div className="row">
-                  <div className="col-md-6 mx-auto text-center add-btn-holder p-3">
-                    <h2 className="mt-2 mb-3"> Your Workouts </h2>
-                    <button
-                      className="btn bg-dark text-light mb-2"
-                      id="add-workout"
-                      data-toggle="modal"
-                      data-target="#form-modal"
-                      onClick={() => {
-                        this.addWorkout();
+      <div className="workouts">
+        <GridContainer style={{ gridTemplateColumns: '1fr 300px' }}>
+          <h4>Your Workouts</h4>
+          <button
+            type="submit"
+            className="btn btn-dark"
+            id="addWorkout"
+            data-toggle="modal"
+            data-target="#form-modal"
+            onClick={() => {
+              this.addWorkout();
+            }}
+          >
+            + Add Workout
+          </button>
+        </GridContainer>
+        <hr></hr>
+        <GridContainer style={{ gridTemplateColumns: '1fr 1fr' }}>
+          {this.state.workouts.map(data => (
+            <CardDiv>
+              <CardBody id={'workoutCard'}>
+                <CardTitle>{data.name}</CardTitle>
+                <GridContainer style={{ gridTemplateColumns: '1fr 1fr' }}>
+                  <div>
+                    <Line
+                      className="chart"
+                      data={{
+                        //Dates of data on the X-axis of the chart.
+                        //Only 3 data points shown for chart clarity.
+                        //Data points are reversed to flow in the proper direction
+                        labels: data.data
+                          .slice(0, 3)
+                          .reverse()
+                          .map(entry => format(new Date(entry.date), 'MM-dd')),
+                        datasets: [
+                          {
+                            label: 'Workout Progress',
+                            scaleStepWidth: 2,
+                            backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            //Values of data on the Y-axis of the chart.
+                            data: data.data
+                              .slice(0, 3)
+                              .reverse()
+                              .map(entry => entry.value),
+                          },
+                        ],
                       }}
-                    >
-                      Add something New
-                    </button>
-                    <hr className="w-100" />
+                      options={{
+                        legend: {
+                          labels: {
+                            fontColor: 'rgba (0, 0, 0, 0.9)',
+                            fontSize: 16,
+                            fontStyle: 'bold',
+                          },
+                        },
+                        scales: {
+                          xAxes: [
+                            {
+                              ticks: {
+                                fontColor: 'rgba(0, 0, 0, 0.8)',
+                                fontStyle: 'bold',
+                              },
+                            },
+                          ],
+                          yAxes: [
+                            {
+                              ticks: {
+                                fontColor: 'rgba(0, 0, 0, 0.8)',
+                                precision: 0,
+                                //Sends the data to cleanStepsize function to dynamically
+                                //create an appropriate amount of y-ticks based on range of data.
+                                //Keeps y-ticks from being clustered together too closely.
+                                stepSize: this.cleanStepsize(data.data),
+                              },
+                            },
+                          ],
+                        },
+                      }}
+                    />
                   </div>
-                </div>
-                <div className="row mt-5" id="workouts-card-container">
-                  <div className="col-md-10 mx-auto text-center">
-                    {/* Generate cards based on Workout data in state */}
-                    {this.state.workouts.map((data) => (
-                      <WorkoutCard
-                        name={data.name}
-                        type={data.type}
-                        description={data.description}
-                        //Only display up to 3 data points on the card itself.
-                        data={data.data.slice(0, 3)}
-                        key={data._id}
-                        id={data._id}
-                        // Saves id to state to prepare for post
-                        delete={() => this.handleDeleteWorkout(data._id)}
-                        addEntry={() => this.addEntry(data._id)}
-                        viewEntries={() => this.viewInfo(data._id, "entries")}
-                        viewChart={() => this.viewInfo(data._id, "chart")}
-                        //Creates Line graph using Chart.js
-                        chart={
-                          <Line
-                            className="chart"
-                            data={{
-                              //Dates of data on the X-axis of the chart.
-                              //Only 3 data points shown for chart clarity.
-                              //Data points are reversed to flow in the proper direction
-                              labels: data.data
-                                .slice(0, 3)
-                                .reverse()
-                                .map((entry) =>
-                                  format(new Date(entry.date), "MM-dd")
-                                ),
-                              datasets: [
-                                {
-                                  label: "Workout Progress",
-                                  scaleStepWidth: 2,
-                                  backgroundColor: "rgb(255, 99, 132)",
-                                  borderColor: "rgb(255, 99, 132)",
-                                  //Values of data on the Y-axis of the chart.
-                                  data: data.data
-                                    .slice(0, 3)
-                                    .reverse()
-                                    .map((entry) => entry.value),
-                                },
-                              ],
-                            }}
-                            options={{
-                              legend: {
-                                labels: {
-                                  fontColor: "rgba (0, 0, 0, 0.9)",
-                                  fontSize: 16,
-                                  fontStyle: "bold",
-                                },
-                              },
-                              scales: {
-                                xAxes: [
-                                  {
-                                    ticks: {
-                                      fontColor: "rgba(0, 0, 0, 0.8)",
-                                      fontStyle: "bold",
-                                    },
-                                  },
-                                ],
-                                yAxes: [
-                                  {
-                                    ticks: {
-                                      fontColor: "rgba(0, 0, 0, 0.8)",
-                                      precision: 0,
-                                      //Sends the data to cleanStepsize function to dynamically
-                                      //create an appropriate amount of y-ticks based on range of data.
-                                      //Keeps y-ticks from being clustered together too closely.
-                                      stepSize: this.cleanStepsize(data.data),
-                                    },
-                                  },
-                                ],
-                              },
-                            }}
-                          />
-                        }
-                      />
-                    ))}
+                  <div>
+                    <CardText>
+                      {data.data ? (
+                        <div>
+                          <h5>Your last 3 entries</h5>
+                          <table className="mx-auto text-center table data-table">
+                            <tbody>
+                              <tr>
+                                <th>Day</th>
+                                <th>{data.type || 'Frequency'}</th>
+                              </tr>
+                              {data.data.slice(0, 3).map(data => {
+                                return (
+                                  <tr>
+                                    <td>
+                                      {format(new Date(data.date), 'MM-dd-yy')}
+                                    </td>{' '}
+                                    <td>{data.value}</td>{' '}
+                                  </tr>
+                                );
+                              })}
+                            </tbody>
+                          </table>
+                          <hr className="w-100" />
+                        </div>
+                      ) : (
+                        //   If no data passed, tell user.
+                        <h3>No data yet!</h3>
+                      )}
+                    </CardText>
+                  </div>
+                </GridContainer>
+              </CardBody>
+              <div className="card-footer">
+                <div className="row">
+                  <div className="col-md-5 d-flex flex-column">
+                    <button
+                      className="btn btn-danger text-light d-flex align-self-start delete-workout-btn"
+                      onClick={() => this.handleDeleteWorkout(data._id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                  <div className="col-md-7 d-flex flex-column">
+                    <div className="card-btn-holder d-flex align-self-end">
+                      <button
+                        className="btn btn-dark text-light view-entries-btn"
+                        onClick={() => this.viewInfo(data._id, 'chart')}
+                        data-toggle="modal"
+                        data-target="#form-modal"
+                      >
+                        Full chart
+                      </button>
+                      <button
+                        className="btn bg-dark text-light ml-3 view-entries-btn"
+                        onClick={() => this.viewInfo(data._id, 'entries')}
+                        data-toggle="modal"
+                        data-target="#form-modal"
+                      >
+                        All entries
+                      </button>
+                      <button
+                        className="btn bg-dark text-light ml-3 add-entry-btn"
+                        onClick={() => this.addEntry(data._id)}
+                        data-toggle="modal"
+                        data-target="#form-modal"
+                      >
+                        Add entry
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </Wrapper>
-          </div>
-        </div>
-      </Container>
+            </CardDiv>
+          ))}
+        </GridContainer>
+        <Modal
+          show={this.state.show}
+          close={this.hideModal}
+          submit={this.handleFormSubmit}
+          //Changes body of modal based on state. Allows for modal reuse.
+          status={this.state.modalForm}
+        >
+          {this.selectForm(this.state.modalForm, this.state.entryID)}
+        </Modal>
+      </div>
     );
   }
 }
